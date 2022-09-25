dd75f190473b7da48a2eb9dca97c0b71d19850c2
Valeriy V Nepluev <greenet2007@gmail.com>
2022-09-25 12:03:31 +0300

    init: ts project

---
+++ ts/01_unit/app.js
@@ -0,0 +1,4 @@
+var revenue = 500;
+var bonus = 1000;
+var res = revenue + bonus;
+console.log(res);
---
+++ ts/01_unit/app.ts
@@ -0,0 +1,6 @@
+let revenue: number = 500
+let bonus: number = 1000
+
+const res: number = revenue + bonus
+
+console.log(res)
